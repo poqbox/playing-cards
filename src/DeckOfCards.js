b7f7ab8893a3d7b46b40e0a27dfb1f16cf876061
poq <120293384+poqbox@users.noreply.github.com>
2025-07-10 14:09:22 -0400

    Made the card codes static

--- src/DeckOfCards.js
+++ src/DeckOfCards.js
@@ -1,17 +1,18 @@
 // https://deckofcardsapi.com/
 
 class DeckData {
-  #deck_id
-  #remaining
-  #shuffled
-  #piles = null
-  #card_back_url = "https://www.deckofcardsapi.com/static/img/back.png"
-  #card_codes = [
+  static card_codes = [
     'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '0C', 'JC', 'QC', 'KC',
     'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '0D', 'JD', 'QD', 'KD',
     'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '0H', 'JH', 'QH', 'KH',
     'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '0S', 'JS', 'QS', 'KS'
   ]
+
+  #deck_id
+  #remaining
+  #shuffled
+  #piles = null
+  #card_back_url = "https://www.deckofcardsapi.com/static/img/back.png"
   #debug_mode
 
   constructor(deck, debug_mode=false) {
@@ -25,7 +26,6 @@ class DeckData {
   getShuffled() { return this.#shuffled }
   getPiles() { return this.#piles }
   getCardBackUrl() { return this.#card_back_url }
-  getCardCodes() { return this.#card_codes }
 
   enableDebugMode() {
     this.#debug_mode = true
